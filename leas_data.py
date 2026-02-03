9a1a7e328f0023f96c6cfec14e743359cd8f8d80
Asbjørn <kykutura@gmx.com>
2026-02-03 13:57:21 +0100

    nu på engels

--- leas_data.py
+++ leas_data.py
@@ -6,53 +6,45 @@ import jax.numpy as jnp
 
 from scipy.io import wavfile
 
-
-
-def til_kartikes_en(a, d):
+def to_cartesian_scalar(a, d):
     return jnp.cos(a)*d
 
-def til_kartikes(polært):
-    x = jnp.cos(polært[:,0])*polært[:,1]
-    y = jnp.sin(polært[:,0])*polært[:,1]
+def to_cartesian(polar):
+    x = jnp.cos(polar[:,0])*polar[:,1]
+    y = jnp.sin(polar[:,0])*polar[:,1]
     return jnp.array([x,y]).T
 
-if len(sys.argv) != 3:
-    print("comandoen skal være på denne form:\n shtipt.py Wide_Long_angle_data.json Wide_Long_distance_data.json")
+if len(sys.argv) != 4:
+    print("Command must be in this form:\n script.py Wide_Long_angle_data.json Wide_Long_distance_data.json Wide_Long_sound.wav")
     sys.exit(1)
 
-## hender data 
-vinkel = json.load(open(sys.argv[1]))
-lengte = json.load(open(sys.argv[2]))
+## Load data
+angle = json.load(open(sys.argv[1]))
+distance = json.load(open(sys.argv[2]))
+sr, x = wavfile.read(sys.argv[3])
 
-pol_data = jnp.array([vinkel["LiDAR_angle"], lengte["LiDAR_distance"]]).T
+polar_data = jnp.array([angle["LiDAR_angle"], distance["LiDAR_distance"]]).T
+audio_data = jnp.append(x[:,0], x[:,1])
+print(audio_data.shape)
 
-## trens former data:
-kartesisk_data = til_kartikes(pol_data)
-# print(jax.make_jaxpr(til_kartikes)(pol_data)) til at kigge på JIT'en
+## Transform data
+cartesian_data = to_cartesian(polar_data)
 
-grad_til_kartikes = jax.jacobian(til_kartikes)
-grad_til_kartikes_en = jax.grad(til_kartikes_en)
+## Gradients
+grad_to_cartesian = jax.jacobian(to_cartesian)
+grad_to_cartesian_scalar = jax.grad(to_cartesian_scalar)
 
-print(grad_til_kartikes)
-print(grad_til_kartikes_en)
+g = grad_to_cartesian_scalar(1.0, 1.0)
+grad_data = grad_to_cartesian(polar_data)
 
+sys.exit(1)
 
-g = grad_til_kartikes_en(1.0,1.0)
-grad_data = grad_til_kartikes(pol_data)
-
-print(g)
-print(grad_data)
-
-#grad_data = grad_til_kartikes(pol_data[:,0])
-#grad_data = grad_til_kartikes(pol_data)
-#grad_data = grad_til_kartikes(pol_data[:,0])
-
-## plotter det 
-x = jnp.array(kartesisk_data[:,0])
-y = jnp.array(kartesisk_data[:,1])
+## Plot data
+x_vals = jnp.array(cartesian_data[:, 0])
+y_vals = jnp.array(cartesian_data[:, 1])
 
 plt.figure()
-plt.scatter(x, y)
+plt.scatter(x_vals, y_vals)
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("LiDAR XY Plot")
