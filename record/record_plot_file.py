dd70c47b227ad449eaa136b83a3fbd180e7a9a16
rasbery py server uni <dedee@deded.dk>
2026-04-21 13:15:42 +0200

    sds

---
+++ record/record_plot_file.py
@@ -0,0 +1,17 @@
+import alsaaudio
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+sr, x = wavfile.read(sound_path)
+
+
+audio = np.frombuffer(x, dtype=np.int16)
+
+fft = np.fft.rfft(audio)
+freqs = np.fft.rfftfreq(len(audio), 1 / RATE)
+
+plt.plot(freqs, np.abs(fft))
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Magnitude")
+plt.show()
