1fc027d7af9d215a36e953de5d06a1bae6159b09
asbjørn <qw41ha@student.aau.dk>
2026-05-13 11:00:46 +0200

    ler er er

--- cnn_data_set_2/data_check.py
+++ cnn_data_set_2/data_check.py
@@ -1,2 +1,13 @@
-    with open("../data_set/data_set_3.csv", "r") as f:
-        rows = [line.strip().split("\t") for line in f]
+with open("data_set/data_set_2.csv", "r") as f:
+    rows = [line.strip().split("\t") for line in f]
+
+#print(rows)
+
+for i in range(0, len(rows), 2):
+    if rows[i][0] != rows[i+1][0] or rows[i][1] != rows[i+1][1]:
+        print(i)
+        print(rows[i])
+        print(rows[i+1])
+
+
+
